520cc4d7c68a2df4ac7aca24a39b7065a4c6ee03
Tringles <50797520+Tringles@users.noreply.github.com>
2020-08-23 02:47:30 +0900

    Add files via upload

---
+++ ps.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define endl '\n'
+#define INF 2e9
+#define LINF 2e11
+#define MOD 1000000007
+
+int n, dp[1000001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	cin >> n;
+	dp[1] = dp[2] = 1;
+	for (int i = 3; i <= n; i++)
+		dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+	cout << dp[n] << endl;
+	return 0;
+}
