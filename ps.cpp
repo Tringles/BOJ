141634ba459e3dc8b5d1c1f69f5cda38aa35d5fc
Tringles <50797520+Tringles@users.noreply.github.com>
2020-08-28 12:06:29 +0900

    Add files via upload

---
+++ ps.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, pair<int, int>> P;
+#define endl '\n'
+#define INF 2e9
+#define LINF 2e11
+#define MOD 1000000007
+
+int n, s, ans;
+vector<int> v;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	v.push_back(-INF);
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		if (v.back() < s) {
+			v.push_back(s);
+			ans++;
+		}
+		else {
+			auto it = lower_bound(v.begin(), v.end(), s);
+			*it = s;
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
