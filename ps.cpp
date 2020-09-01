f117dcb9f67f865dd9442d09eabed3b1acf482d5
Tringles <50797520+Tringles@users.noreply.github.com>
2020-09-01 23:52:30 +0900

    Add files via upload

---
+++ ps.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PL;
+typedef pair<int, pair<int, int>> PII;
+typedef pair<ll, pair<ll, ll>> PLL;
+#define endl '\n'
+#define INF 2e9
+#define LINF 2e15
+#define MOD 1000000007
+
+ll n, ans, k = 10, cnt = 1;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		if (i / k) {
+			k *= 10;
+			cnt++;
+		}
+		ans += cnt;
+	}
+	cout << ans << endl;
+	return 0;
+}
