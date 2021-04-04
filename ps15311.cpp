1d41ec6fc5e594cc2ed3cfa0388d3707de82c8e2
Tringles <jjmjm2002@naver.com>
2021-04-04 19:19:23 +0900

    15311

---
+++ ps15311.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PL;
+typedef pair<int, pair<int, int>> PII;
+typedef pair<ll, pair<ll, ll>> PLL;
+#define endl '\n'
+#define INF 2e9
+#define LINF 2e15
+#define MOD 1000000007
+
+int n;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    cin >> n;
+    cout << 1999 << endl;
+    for(int i = 0; i < 999; i ++)cout << 1 << ' ';
+    for(int i = 0; i < 1000; i++)cout << 1000 << ' ';
+    return 0;
+}
