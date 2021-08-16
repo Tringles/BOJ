4eaf5db702afb01008af2454133d79e41674a6d1
Tringles <jjmjm2002@naver.com>
2021-08-16 18:15:25 +0900

    21598

---
+++ ps21598.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PL;
+typedef pair<int, pair<int, int>> PII;
+typedef pair<ll, pair<ll, ll>> PLL;
+#define endl '\n'
+#define INF 2e9
+#define LINF 2e15
+#define MOD 1000000007
+
+int n;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> n;
+	while (n--)cout << "SciComLove" << endl;
+	return 0;
+}
