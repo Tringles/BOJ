c28c00020fcfb9adbc8739b5cc7caa961eda51e8
Tringles <jjmjm2002@naver.com>
2021-11-27 21:59:02 +0900

    2965

---
+++ ps2965.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PL;
+typedef pair<int, pair<int, int>> PII;
+typedef pair<ll, pair<ll, ll>> PLL;
+#define endl '\n'
+#define INF 2e9
+#define LINF 2e15
+#define MOD 1000000007
+
+int a, b, c;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	cin >> a >> b >> c;cout << max(b - a, c - b) - 1 << endl;
+	return 0;
+}
